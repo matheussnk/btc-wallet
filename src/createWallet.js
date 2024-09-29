cbaff46ff9311656b7ec04c02b9d607bcfc574d1
Matheus <lima.mm2011@gmail.com>
2024-09-28 22:49:05 -0300

    Delivering the challenge

--- src/createWallet.js
+++ src/createWallet.js
@@ -4,8 +4,6 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 const network = bitcoin.networks.testnet
 const path = `m/49'/1'/0'/0`
-
-//criando o mnemonic para a seed (palavras de senha)
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 let root = bip32.fromSeed(seed, network)
